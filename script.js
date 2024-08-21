82d9cf3e3f6cf03fe9765fefba85d6a2e7f427f0
Kiril Yanev <kl_yanev@abv.bg>
2024-08-22 00:43:26 +0300

    feat: fetch quotes data from the api

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('testing')
+let apiQuotes = {};
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes.results[Math.floor(Math.random() * apiQuotes.count)]
+    console.log(quote.content)
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://api.quotable.io/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
